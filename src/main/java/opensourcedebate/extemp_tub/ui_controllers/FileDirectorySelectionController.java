f44f71fdc9d500376646cafce2bbcd549d738926
ProgressingDebate <trentonbeckendorff@texasbb.com>
2015-01-24 11:55:49 -0600

    Added Partial Navigation Selection

--- src/main/java/opensourcedebate/extemp_tub/ui_controllers/FileDirectorySelectionController.java
+++ src/main/java/opensourcedebate/extemp_tub/ui_controllers/FileDirectorySelectionController.java
@@ -1,8 +1,11 @@
 package opensourcedebate.extemp_tub.ui_controllers;
 
+import java.io.File;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -10,10 +13,15 @@ public class FileDirectorySelectionController {
 	@FXML private Button OpenFileNavigation;
 	
 	@FXML protected void fileNavigation(ActionEvent event) {
+		File default_directory = new File(System.getenv("USERPROFILE"));
+		System.out.println("Default System Path: " + default_directory.toString());
+		
 		//http://docs.oracle.com/javase/8/javafx/user-interface-tutorial/file-chooser.htm#CCHICECF
-		FileChooser fileChooser = new FileChooser();
-		fileChooser.setTitle("Open Resource File");
-		fileChooser.showOpenDialog((Stage) OpenFileNavigation.getScene().getWindow());
+		DirectoryChooser directoryChooser = new DirectoryChooser();
+		directoryChooser.setTitle("Select a File Directory");
+		directoryChooser.setInitialDirectory(default_directory);
+		directoryChooser.showDialog((Stage) OpenFileNavigation.getScene().getWindow());
+		
 	}
 	
 	@FXML protected void onContinue(ActionEvent event) {
